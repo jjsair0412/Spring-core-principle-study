63dbd4b492f8244a4d563615994ba72e476d98ec
주진성 <jjsair0412@naver.com>
2021-07-25 21:01:28 +0900

    Spring 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,33 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+// Configuration 어노테이션을 달아준 클래스는, 스프링 설정파일이 된다.
 public class AppConfig {
 
 
+    @Bean
+    // Bean 어노테이션을 작성해서 bean객체로 만들어준다.
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
 //        return new FixDiscountPolicy();
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,14 +3,25 @@ package hello.core;
 import hello.core.Member.Grade;
 import hello.core.Member.Member;
 import hello.core.Member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args){
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // ApplicationContext 객체를 생성해주는데, AnnotationConfigApplicationContext()안의 파라미터값으로
+        // 아까 스프링 설정파일로 만들어주었던 AppConfig.class를 넣어준다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // getBean하여 설정파일에 있던 bean객체 이름과 클래스를 차례대로 작성해 넣어주고,
+        // 데이터타입에 맞춰 객체를 생성해준다.
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
+        // 생성해줫던 객체를 참조연산자를 이용해 사용한다.
         memberService.join(member);
 
         Member findmember = memberService.findMember(1L);
