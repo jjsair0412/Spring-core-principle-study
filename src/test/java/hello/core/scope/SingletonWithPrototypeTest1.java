704747e6c281b572cd80e25e7f3befb6d369c970
주진성 <jjsair0412@naver.com>
2021-07-30 18:03:44 +0900

    Singleton Scope와 Prototype Scope을 같이 사용했을때 문제점 - 해결방안, Provider

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,12 +2,14 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -36,7 +38,13 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean1.logic();
-        assertThat(count2).isEqualTo(2);
+        /**
+        assertThat(count2).isEqualTo(2);**/
+
+        /**
+         * ObjectProvider를 사용해서 항상 새로운 bean을 반환하게끔 했다.
+         */
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
@@ -44,14 +52,38 @@ public class SingletonWithPrototypeTest1 {
         // 생성시점에 주입되기 때문에 계속 같은 변수를 사용한다.
         private final PrototypeBean prototypeBean;
 
+        /**
+         * ObjectProvider는 <>안에 들어가있는 bean을 찾아주기만 한다.
+         * ObjectFactory를 사용해도 무관하다.
+         @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+         */
+
+        /**
+         * 3번쨰 방법인 Provider를 활용한 방법.
+         * javax.provider를 사용한다.
+         * ObjectProvider와 차이점은 getObject()였는데
+         * 얘는 get으로 컨테이너에서 bean을 찾아와준다.
+         */
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
         @Autowired
         public ClientBean(PrototypeBean prototypeBean){
             this.prototypeBean = prototypeBean;
         }
 
         public int logic(){
+            /**
+            PrototypeBean beanProviderObject = prototypeBeanProvider.getObject();
+
             prototypeBean.addCount();
             return prototypeBean.getCount();
+             **/
+            PrototypeBean beanProviderObject = prototypeBeanProvider.get();
+
+            beanProviderObject.addCount();
+            return beanProviderObject.getCount();
         }
     }
 
