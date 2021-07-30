9d4b2535ba888235fa6d65709e92b7bf9dc29b11
주진성 <jjsair0412@naver.com>
2021-07-30 12:13:45 +0900

    Singleton Scope와 Prototype Scope을 같이 사용했을때 문제점

---
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,81 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void prototypeFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean bean1 = ac.getBean(PrototypeBean.class);
+        bean1.addCount();
+        assertThat(bean1.getCount()).isEqualTo(1);
+
+        PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
+        bean2.addCount();
+        assertThat(bean1.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUsePrototype(){
+        // 둘다 컴포넌트 스캔을 해줘야 bean등록이 되어서 자동주입이 된다.
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class,PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean1.logic();
+        assertThat(count2).isEqualTo(2);
+    }
+
+    @Scope("singleton")
+    static class ClientBean{
+        // 생성시점에 주입되기 때문에 계속 같은 변수를 사용한다.
+        private final PrototypeBean prototypeBean;
+
+        @Autowired
+        public ClientBean(PrototypeBean prototypeBean){
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic(){
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+
+
+    @Scope("prototype")
+    static class  PrototypeBean{
+        private int count = 0;
+
+        public void addCount(){
+            count++;
+        }
+
+        public int getCount(){
+            return count;
+        }
+
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init" + this);
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("prototypeBean.destroy");
+        }
+    }
+}
